81a3a630b94e25114b8dec3823a33ba051eb5b2e
Cheeseum <z.cheeseum@gmail.com>
2014-05-28 14:45:09 -0700

    simple admin plugin

---
+++ plugins/admin.py
@@ -0,0 +1,19 @@
+def irc_public (client, user, channel, message):
+    if client.access_list.check(user, client.access_list.LEVEL_OWNER):
+        if message.startswith('!'):
+            args = message.split(' ')
+            cmd = args.pop(0)
+           
+            if cmd == '!access':
+                client.access_list.add(args[0],  int(args[1]))
+                client.say(channel, 'Added mask {0} with level {1}'.format(args[0], args[1]))
+
+            if cmd == '!join':
+                client.join(args[0])
+            
+            if cmd == '!leave':
+                client.leave(channel)
+            
+            if cmd == '!reload':
+                client.reload_plugins()
+                client.say(channel, 'Plugins Reloaded!')

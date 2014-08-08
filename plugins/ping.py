93c9ac41b02bf4173fc829e4039b1f4da44df2f9
Cheeseum <z.cheeseum@gmail.com>
2014-08-07 20:45:55 -0700

    add more silly messages to ping

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,5 +1,5 @@
 import random
-responses = ["pong", "butts", "bing", "bong", "dong", "wong", "gong"]
+responses = ["pong", "butts", "bing", "bong", "dong", "wong", "gong", "ding", "wing", "ring", "that's gnu PLUS ping to you", "kentucky fried pingen", "burger ping", "ping of the hill"]
 
 def irc_public (client, user, channel, message):
     if message.strip() == "ping":

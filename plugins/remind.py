ad0fd9cb76a8471e1970ad71d370e5ac144a02e3
Cheeseum <z.cheeseum@gmail.com>
2014-11-11 15:23:43 -0500

    fix remind plugin being bad

--- plugins/remind.py
+++ plugins/remind.py
@@ -10,10 +10,16 @@ reminders = defaultdict(list)
 pdt = parsedatetime.Calendar()
 
 def init ():
+    global reminders # FIXME: PUKE
     reminders = data_writer.get("reminders.db")
 
 def client_connected (client):
-    reminders[client.server][:] = [r for r in reminders[client.server] if schedule_reminder(client, r)]
+    # this runs on every connect, but it's okay since we get a new client object every time 
+    global reminders # FIXME: PUKE
+
+    # purge reminders that have passed
+    # FIXME: be more intelligent about this
+    reminders[client.server] = [ r for r in reminders[client.server] if schedule_reminder(client, r) ]
     save(client)
 
 def save (client):
@@ -22,9 +28,8 @@ def save (client):
 def schedule_reminder (client, reminder):
     nick, t, channel, remindtime, remindmsg = reminder
     reminddelta = remindtime - time.time()
-
-    if reminddelta > 0:
-        reminders[client.server].append(reminder)
+    
+    if reminddelta > 0: # the future
         client.schedule(reminddelta, client.say, channel, "{0}: Sent {1}: <{2}> {3}".format(
             t, humanize.naturaltime(reminddelta), nick, remindmsg
         ))
@@ -53,7 +58,9 @@ def irc_public (client, hostmask, channel, message):
                 remindmsg = (msg[:spos] + msg[epos:]).strip()
 
                 reminddelta = remindtime - time.time()
-                if schedule_reminder(client, (nick, t, channel, remindtime, remindmsg)):
+                reminder = (nick, t, channel, remindtime, remindmsg)
+                if schedule_reminder(client, reminder):
+                    reminders[client.server].append(reminder)
                     save(client)
                     client.say(channel, "Okay, I'll remind {0} then!".format(t))
                 else:

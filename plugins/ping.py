a7ca47d554903ff7aeb40bb8157081680d6e4088
Cheeseum <z.cheeseum@gmail.com>
2015-11-21 15:50:39 -0500

    bong correctly

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,14 +1,19 @@
 import random
 import datetime
+import pytz
 
-responses = ["pong", "butts", "bing", "bong", "dong", "wong", "gong", "ding", "wing", "ring", "that's gnu PLUS ping to you", "kentucky fried pingen", "burger ping", "ping of the hill"]
+responses = ["pong", "butts", "bing", "bong", "dong", "wong", "gong", "ding", "wing", "ring", "that's gnu PLUS ping to you", "kentucky fried pingen", "burger ping", "ping of the hill", "xi jinping"]
+
+bongzone = pytz.timezone('Europe/London')
 
 def irc_public (client, user, channel, message):
     if message.strip('!#?.~') == "ping":
         client.say(channel, random.choice(responses))
 
     if message.strip('!#?.~') == 'bing':
-        bongs = datetime.datetime.utcnow().hour
+        # i hate timezones
+        bongs = bongzone.normalize(pytz.utc.localize(datetime.datetime.utcnow()).astimezone(bongzone)).hour
+        #bongs = datetime.datetime.utcnow().hour
         if bongs > 12:
             bongs = bongs - 12
 
